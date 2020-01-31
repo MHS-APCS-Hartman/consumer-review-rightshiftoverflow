d63976343ac85cc08e71d6bfbd1d9ffdb7fcdeb2
rightshiftoverflow <cocoanut99@gmail.com>
2020-01-31 10:29:08 -0500

    Update

--- TestReview.java
+++ TestReview.java
@@ -1,20 +1,27 @@
 public class TestReview {
   public static void main(String[] args)
   {
-    System.out.println(Review.totalSentiment("SimpleReview.txt")); 
+    System.out.println("Simple Review, totalSentiment: "+ Review.totalSentiment("SimpleReview.txt")); 
     //The above line prints -2.9200000000000004
     
-    System.out.println(Review.starRating("SimpleReview.txt")); 
+    System.out.println("Simple Review, starRating: " + Review.starRating("SimpleReview.txt") + "\n"); 
     //The above line prints 1
+        
     
-    System.out.println(Review.totalSentiment2("SimpleReview.txt"));
+    //System.out.println(Review.totalSentiment2("SimpleReview.txt"));
     //The above line prints -0.13904761904761906
     
-     System.out.println(Review.totalSentiment("26WestReview.txt")); 
+     System.out.println("26WestReview, totalSentiment: " + Review.totalSentiment("26WestReview.txt")); 
     //The above line prints 29.04999999999999
     
-     System.out.println(Review.starRating("26WestReview.txt")); 
+     System.out.println("26WestReview, starRating: " + Review.starRating("26WestReview.txt") + "\n"); 
     //The above line prints 4
-     
+    
+    System.out.println("Good Review, fakeReview: " + Review.fakeReview("good_starred.txt"));
+    System.out.println("Bad Review, fakeReview: " + Review.fakeReview("bad_starred.txt") + "\n");
+  
+     System.out.println("Good Review, fakeReviewStronger: " + Review.fakeReviewStronger("good_starred.txt"));
+    System.out.println("Bad Review, fakeReviewStronger: " + Review.fakeReviewStronger("bad_starred.txt") + "\n");
+    
   }
 }
